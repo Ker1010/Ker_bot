be96a381bc62c01eb2b077220d82410fc0800cf0
Ker <kermenglee@gmail.com>
2022-07-29 20:43:21 +0800

    sceenshot uosate

--- commands/crash.js
+++ commands/crash.js
@@ -3,6 +3,7 @@ module.exports = {
     description: "crash",
     execute(message, args){
         if(message.member.roles.cache.has('774974794017406986')){
+            message.channel.send('crashed');
             message.channel.send(message.channel.send({ embeds: [newEmbed] }))
         } else {
             message.channel.send(`:no_entry_sign: **${message.author.username}**，你不能使用它`)
--- node_modules/discord-minigames/src/index.js
+++
@@ -1,6 +0,0 @@
-//BATTLES
-const {createBattle} = require("./battle")
-module.exports.startBattle = async function (member, message) { return createBattle(member, message) }
-//I SPYS
-const {createISpy} = require("./ispy")
-module.exports.ISpy = createISpy
